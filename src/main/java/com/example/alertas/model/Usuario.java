1494729e4f36c6b83db9fb061f41c9ec1feed675
Federico <federicovillarreal396@gmail.com>
2025-07-10 23:00:00 -0300

    fix usuario

--- src/main/java/com/example/alertas/model/Usuario.java
+++ src/main/java/com/example/alertas/model/Usuario.java
@@ -5,18 +5,41 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "usuarios")
 public class Usuario {
+
     @Id
     private String id;
     private String username;
     private String password;
 
-    // Getters y setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public Usuario() {
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
